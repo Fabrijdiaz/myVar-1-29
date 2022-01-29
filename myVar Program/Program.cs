a05990df30f173f991fbf45370d18d2fc83ffaab
Fabrijdiaz <fabrijdiaz@gmail.com>
2022-01-29 17:26:12 -0600

    Initial Commit

---
+++ myVar Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace myVar_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            double myVar = 7;
+
+            // Process
+            Console.WriteLine(-1 + 4 * myVar);
+            Console.WriteLine((35 + 5) % myVar);
+            Console.WriteLine(14 + -4 * 6 / 12);
+            Console.WriteLine(2 + 12 / 6 * 1 - myVar % 2);
+
+            //Output
+            if (myVar * myVar < 10)
+            {
+                Console.WriteLine("\nmyVar is less than 4");
+            }
+            else
+            {
+                Console.WriteLine("\nmyVar is greater than 4");
+            }
+        }
+    }
+}
